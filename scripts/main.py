e938a31afcb2c46760d5ec699ca5a7309a0b9e58
JMJHOX <josemiguelaparicio507@gmail.com>
2022-11-26 22:38:52 -0500

    advance

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,36 @@
 
 import tabula
 import pandas as pd
-# CONVERTING DATA TO CSV
+
+
+def clean_data(row):
+    row_replacement = row.str.split('\r', 1)
+    row_cleaned = []
+    for x in row_replacement:
+        if (len(x) > 1):
+            separate_date_1 = ''.join(x[0])
+            separate_date_2 = ''.join(x[1])
+            row_cleaned.append(separate_date_1)
+            row_cleaned.append(separate_date_2)
+        if (len(x) == 1):
+            row_cleaned.append(''.join(x))
+    return row_cleaned
+
+
+# Rating from 0 to 7
+def rate_province(provinces_row):
+    row_rated = []
+    for row in provinces_row:
+        row_value = int(row)
+        if (row == 0):
+            row_rated.append(0)
+        if (row_value >= 1 and row_value <= 5):
+            row_rated.append(1)
+        if (row_value >= 5 and row_value <= 10):
+            row_rated.append(2)
+        if (row_value >= 10):
+            row_rated.append(3)
+    return row_rated
 
 
 def save_df(df):
@@ -32,34 +61,34 @@ df = tabula.read_pdf(
     lattice=True
 )
 
+# Cleaning and generating data
+processed_data = []
+rated_provinces = []
+for column in df[0].columns:
+    processed_data.append(clean_data(df[0][column]))
 
-# Using list(df) to get the list of all Column Names
-print("check:")
-print(df[0]['Unnamed: 1'])
-row_replacement = df[0]['Unnamed: 1'].str.split('\r', 1)
-print("replacement:")
-print(row_replacement)
-data = [['tom', 10], ['nick', 15], ['juli', 14]]
 
-append_arr_test = []
-i = 0
-for x in row_replacement:
+rated_provinces = rate_province(processed_data[1])
 
-    append_arr_test[i] = x[0]
-    if (len(x) > 1):
-        print("bbblb")
-    i += 1
-print("finish")
-print(append_arr_test)
-append_arr = pd.DataFrame([[append_arr_test]])
-print("finish2")
-print(append_arr)
+append_arr = pd.DataFrame({'provinces': processed_data[0], 'total': processed_data[1],
+                           'violence_rating': rated_provinces})
+print("finish2", append_arr)
+# convert PDF into CSV
+append_arr.to_json("pandita.json",orient='records')
+#append_arr.to_csv('test.csv', encoding='cp1252')
+
+#''.join([''.join(l) for l in  df[0]['Unnamed: 1'].str.split('\r', 1)])
+#df[0]['Unnamed: 1'].str.split('\r', 1)
+
+
+#results= clean_data(df[0]['Unnamed: 1'])
+#
+# print("finish",processed_data)
 #df[0]['Unnamed: 1'] = row_replacement.str.extract(r'(,)/g', expand=True)
 #check_data = df[0]['Unnamed: 1'].str.split(r'/(\\r)/g', expand=True)
 #print(df[0]['Unnamed: 1'])
 #df['code'] = df['TOTAL.............'].str.extract(r'\\r')
-# convert PDF into CSV
-#df.to_csv('test.csv', encoding='cp1252')
+
 # save_df(df)
 # print(df)
 # CLEANING DATA PER CSV
